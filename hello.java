a7dbdab01b63af7fde6bb81eac3cf3078d0d4834
Ishita Vaka <ishita.vaka@gmail.com>
2026-03-14 12:41:01 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("Hello, World!");
+        int x = 3, y = 5;
+        System.out.println(x + y);
+    }
+}
